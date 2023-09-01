aa611a62a79b4403c9ce6fb05628c291d733b51d
elevenstand7 <elevenstand7@gmail.com>
2023-09-01 15:37:53 -0400

    up to return part in form

--- intro-to-react-forms/src/form.jsx
+++ intro-to-react-forms/src/form.jsx
@@ -4,10 +4,12 @@ function Form() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
-    const [bio, setBio] = useState("")
-
+    const [phoneType, setPhoneType] = useState(null);
+    const [staff, setStaff] = useState(null);
+    const [bio, setBio] = useState("");
+    const [emailNotif, setEmailNotif] = useState(null);
     const [errorMessages, setErrorMessages] = useState([]);
-    // this is a git comment
+
 
     const validate = () => {
         const errors = [];
@@ -19,8 +21,11 @@ function Form() {
         if (!email.includes('@')) {
             errors.push("Email must be in a valid format.");
         }
-        if (phoneNumber && phoneNumber.length < 10) {
-            errors.push("Phone Number must be at least 10 numbers.");
+        if (phoneNumber && (!/^\d{3}-?\d{3}-?\d{4}$/.test(phoneNumber))) {
+            errors.push("Phone Number invalid and should be in format ###-###-####.");
+        }
+        if(phoneNumber && phoneType === null){
+            errors.push("Please select phone type.")
         }
         if (bio.length > 280) {
             errors.push("Bio exceeded 280 character limit")
@@ -30,9 +35,77 @@ function Form() {
     };
 
 
+    const handleChange = (field) => {
+        return (e)=>{
+            switch(field){
+                case 'name':
+                    setName(e.target.value);
+                    break;
+                case 'email':
+                    setEmail(e.target.value);
+                    break;
+                case 'phoneNumber':
+                    setPhoneNumber(e.target.value);
+                    break;
+                case 'phoneType':
+                    setPhoneType(e.target.value);
+                case 'staff':
+                    setStaff(e.target.value);
+                    break;
+                case 'bio':
+                    setBio(e.target.value);
+                    break;
+                case 'emailNotif':
+                    setEmailNotif(e.target.value);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+        let errors = [];
+
+        if(errors.length){
+            setErrorMessages(errors);
+        }else{
+            let user = {
+                name,
+                email,
+                phoneNumber,
+                phoneType,
+                staff,
+                bio,
+                emailNotif
+            };
+
+            setErrorMessages([]);
+        }
+    };
+
+    const showErrors = ()=>{
+        if(!errorMessages.length){
+            return null;
+        }else{
+            return(
+                <ul>
+                    {errorMessages.map( error => <li>error</li>)}
+                </ul>
+            )
+        }
+    }
+
+
+    return (
+        <>
+            {showErrors()}
+
+        </>
+    )
 
 };
 
 
-const test = 1;
